02727a8adf8e0b2da6a9e98f66fecd1727f918b1
dksharma <sharma.deepak2@mayo.edu>
2023-06-22 18:16:23 -0500

    Updated for xhtml StructureDefintion

--- org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/conformance/ShExGenerator.java
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/conformance/ShExGenerator.java
@@ -119,7 +119,7 @@ public class ShExGenerator {
   private List<String> shortIdException = Arrays.asList(
     "base64Binary", "boolean",
     "date", "dateTime", "decimal", "instant", "integer64",
-    "integer", "string", "time", "uri"
+    "integer", "string", "time", "uri", "xhtml"
   );
 
   private List<String> mappedFunctions = Arrays.asList(
@@ -618,7 +618,8 @@ public class ShExGenerator {
    */
   private String genShapeDefinition(StructureDefinition sd, boolean top_level) {
     // xhtml is treated as an atom
-    if("xhtml".equals(sd.getName()) || (completeModel && "Resource".equals(sd.getName())))
+    //if("xhtml".equals(sd.getName()) || (completeModel && "Resource".equals(sd.getName())))
+    if(completeModel && "Resource".equals(sd.getName()))
       return "";
 
     ST shape_defn;
@@ -1393,7 +1394,7 @@ public class ShExGenerator {
     } else {
       if (ed.getType().size() == 1) {
         // Single entry
-        if ((defn.isEmpty())||(typ.equals(sd.getName())))
+        if ((defn.isEmpty())||(typ.equals(sd.getName()))||("xhtml".equals(sd.getName())))
           defn = genTypeRef(sd, ed, id, ed.getType().get(0));
       } else if (ed.getContentReference() != null) {
         // Reference to another element
@@ -1596,6 +1597,9 @@ public class ShExGenerator {
 
     } else if (typ.getCode().startsWith(Constants.NS_SYSTEM_TYPE)) {
       String xt = getShexCode(typ.getWorkingCode());
+
+      if ("xhtml.value".equals(id))
+        xt = "rdf:XMLLiteral";
 
       // TODO: Remove the next line when the type of token gets switched to string
       // TODO: Add a rdf-type entry for valueInteger to xsd:integer (instead of int)
